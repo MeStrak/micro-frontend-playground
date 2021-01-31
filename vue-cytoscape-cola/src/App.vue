ee3781a57dbd60355cb28e51571881e0006b4ac2
MeStrak <call_me_nigel@yahoo.co.uk>
2021-01-31 16:12:58 +0000

    style: auto format

--- vue-cytoscape-cola/src/App.vue
+++ vue-cytoscape-cola/src/App.vue
@@ -17,658 +17,658 @@ const config = {
 
   boxSelectionEnabled: false,
   layout: {
-    name: "cola"
+    name: "cola",
   },
   style: [
     {
       selector: "node",
       css: {
-        "background-color": "#f92411"
-      }
+        "background-color": "#f92411",
+      },
     },
     {
       selector: "edge",
       css: {
-        "line-color": "#f92411"
-      }
-    }
+        "line-color": "#f92411",
+      },
+    },
   ],
   elements: {
     nodes: [
       {
         data: {
           id: "1",
-          label: "P"
-        }
+          label: "P",
+        },
       },
       {
         data: {
           id: "2",
-          label: "sucrose phosphate phosphatase"
-        }
+          label: "sucrose phosphate phosphatase",
+        },
       },
       {
         data: {
           id: "4",
-          label: "sucrose 6-phosphate"
-        }
+          label: "sucrose 6-phosphate",
+        },
       },
       {
         data: {
           id: "6",
-          label: "sucrose"
-        }
+          label: "sucrose",
+        },
       },
       {
         data: {
           id: "8",
-          label: "invertase"
-        }
+          label: "invertase",
+        },
       },
       {
         data: {
           id: "10",
-          label: "fructose"
-        }
+          label: "fructose",
+        },
       },
       {
         data: {
           id: "12",
-          label: "fructokinase"
-        }
+          label: "fructokinase",
+        },
       },
       {
         data: {
           id: "14",
-          label: "fructose 6-phosphate"
-        }
+          label: "fructose 6-phosphate",
+        },
       },
       {
         data: {
           id: "20",
-          label: "phosphoglucose isomerase"
-        }
+          label: "phosphoglucose isomerase",
+        },
       },
       {
         data: {
           id: "22",
-          label: "glucose 6-phosphate"
-        }
+          label: "glucose 6-phosphate",
+        },
       },
       {
         data: {
           id: "28",
-          label: "glucose"
-        }
+          label: "glucose",
+        },
       },
       {
         data: {
           id: "30",
-          label: "hexokinase"
-        }
+          label: "hexokinase",
+        },
       },
       {
         data: {
           id: "33",
-          label: "sucrose synthase"
-        }
+          label: "sucrose synthase",
+        },
       },
       {
         data: {
           id: "36",
-          label: "UDP - glucose"
-        }
+          label: "UDP - glucose",
+        },
       },
       {
         data: {
           id: "38",
-          label: "sucrose phosphate synthase"
-        }
+          label: "sucrose phosphate synthase",
+        },
       },
       {
         data: {
           id: "41",
-          label: "UDP"
-        }
+          label: "UDP",
+        },
       },
       {
         data: {
           id: "44",
-          label: "fructose 6-phosphate"
-        }
+          label: "fructose 6-phosphate",
+        },
       },
       {
         data: {
           id: "46",
-          label: "ATP"
-        }
+          label: "ATP",
+        },
       },
       {
         data: {
           id: "47",
-          label: "ATP"
-        }
+          label: "ATP",
+        },
       },
       {
         data: {
           id: "52",
-          label: "ATP"
-        }
+          label: "ATP",
+        },
       },
       {
         data: {
           id: "57",
-          label: "ADP"
-        }
+          label: "ADP",
+        },
       },
       {
         data: {
           id: "66",
-          label: "PP"
-        }
+          label: "PP",
+        },
       },
       {
         data: {
           id: "71",
-          label: "UTP"
-        }
+          label: "UTP",
+        },
       },
       {
         data: {
           id: "76",
-          label: "UDP glucose pyrophosphorylase"
-        }
+          label: "UDP glucose pyrophosphorylase",
+        },
       },
       {
         data: {
           id: "80",
-          label: "glucose 1-phosphate"
-        }
+          label: "glucose 1-phosphate",
+        },
       },
       {
         data: {
           id: "86",
-          label: "phospho- glucomutase (cPGM)"
-        }
+          label: "phospho- glucomutase (cPGM)",
+        },
       },
       {
         data: {
           id: "89",
-          label: "G1P transporter"
-        }
+          label: "G1P transporter",
+        },
       },
       {
         data: {
           id: "90",
-          label: "P"
-        }
+          label: "P",
+        },
       },
       {
         data: {
           id: "95",
-          label: "P"
-        }
+          label: "P",
+        },
       },
       {
         data: {
           id: "102",
-          label: "P"
-        }
+          label: "P",
+        },
       },
       {
         data: {
           id: "103",
-          label: "P"
-        }
+          label: "P",
+        },
       },
       {
         data: {
           id: "104",
-          label: "G6P transporter"
-        }
+          label: "G6P transporter",
+        },
       },
       {
         data: {
           id: "109",
-          label: "glucose 6-phosphate"
-        }
+          label: "glucose 6-phosphate",
+        },
       },
       {
         data: {
           id: "115",
-          label: "phospho- glucomutase (cPGM)"
-        }
+          label: "phospho- glucomutase (cPGM)",
+        },
       },
       {
         data: {
           id: "121",
-          label: "glucose 1-phosphate"
-        }
+          label: "glucose 1-phosphate",
+        },
       },
       {
         data: {
           id: "128",
-          label: "ADPglucose pyrophosphorylase (pAGPase)"
-        }
+          label: "ADPglucose pyrophosphorylase (pAGPase)",
+        },
       },
       {
         data: {
           id: "130",
-          label: "ADP - glucose"
-        }
+          label: "ADP - glucose",
+        },
       },
       {
         data: {
           id: "136",
-          label: "PP"
-        }
+          label: "PP",
+        },
       },
       {
         data: {
           id: "141",
-          label: "ATP"
-        }
+          label: "ATP",
+        },
       },
       {
         data: {
           id: "148",
-          label: "inorganic diphosphatase"
-        }
+          label: "inorganic diphosphatase",
+        },
       },
       {
         data: {
           id: "149",
-          label: "P"
-        }
+          label: "P",
+        },
       },
       {
         data: {
           id: "156",
-          label: "phosphate transporter"
-        }
+          label: "phosphate transporter",
+        },
       },
       {
         data: {
           id: "158",
-          label: "P"
-        }
+          label: "P",
+        },
       },
       {
         data: {
           id: "164",
-          label: "starch synthase (simpl.)"
-        }
+          label: "starch synthase (simpl.)",
+        },
       },
       {
         data: {
           id: "166",
-          label: "ADP"
-        }
+          label: "ADP",
+        },
       },
       {
         data: {
           id: "172",
-          label: "starch"
-        }
+          label: "starch",
+        },
       },
       {
         data: {
           id: "178",
-          label: "ATP/ADP transporter"
-        }
+          label: "ATP/ADP transporter",
+        },
       },
       {
         data: {
           id: "179",
-          label: "ADP"
-        }
+          label: "ADP",
+        },
       },
       {
         data: {
           id: "184",
-          label: "ADP"
-        }
+          label: "ADP",
+        },
       },
       {
         data: {
           id: "189",
-          label: "ATP"
-        }
-      }
+          label: "ATP",
+        },
+      },
     ],
     edges: [
       {
         data: {
           source: "2",
-          target: "1"
-        }
+          target: "1",
+        },
       },
       {
         data: {
           source: "4",
-          target: "2"
-        }
+          target: "2",
+        },
       },
       {
         data: {
           source: "2",
-          target: "6"
-        }
+          target: "6",
+        },
       },
       {
         data: {
           source: "6",
-          target: "8"
-        }
+          target: "8",
+        },
       },
       {
         data: {
           source: "8",
-          target: "10"
-        }
+          target: "10",
+        },
       },
       {
         data: {
           source: "12",
-          target: "14"
-        }
+          target: "14",
+        },
       },
       {
         data: {
           source: "14",
-          target: "20"
-        }
+          target: "20",
+        },
       },
       {
         data: {
           source: "20",
-          target: "22"
-        }
+          target: "22",
+        },
       },
       {
         data: {
           source: "8",
-          target: "28"
-        }
+          target: "28",
+        },
       },
       {
         data: {
           source: "28",
-          target: "30"
-        }
+          target: "30",
+        },
       },
       {
         data: {
           source: "30",
-          target: "22"
-        }
+          target: "22",
+        },
       },
       {
         data: {
           source: "6",
-          target: "33"
-        }
+          target: "33",
+        },
       },
       {
         data: {
           source: "33",
-          target: "10"
-        }
+          target: "10",
+        },
       },
       {
         data: {
           source: "33",
-          target: "36"
-        }
+          target: "36",
+        },
       },
       {
         data: {
           source: "36",
-          target: "38"
-        }
+          target: "38",
+        },
       },
       {
         data: {
           source: "38",
-          target: "4"
-        }
+          target: "4",
+        },
       },
       {
         data: {
           source: "38",
-          target: "41"
-        }
+          target: "41",
+        },
       },
       {
         data: {
           source: "41",
-          target: "33"
-        }
+          target: "33",
+        },
       },
       {
         data: {
           source: "44",
-          target: "38"
-        }
+          target: "38",
+        },
       },
       {
         data: {
           source: "52",
-          target: "12"
-        }
+          target: "12",
+        },
       },
       {
         data: {
           source: "12",
-          target: "57"
-        }
+          target: "57",
+        },
       },
       {
         data: {
           source: "46",
-          target: "30"
-        }
+          target: "30",
+        },
       },
       {
         data: {
           source: "30",
-          target: "47"
-        }
+          target: "47",
+        },
       },
       {
         data: {
           source: "71",
-          target: "76"
-        }
+          target: "76",
+        },
       },
       {
         data: {
           source: "76",
-          target: "66"
-        }
+          target: "66",
+        },
       },
       {
         data: {
           source: "76",
-          target: "36"
-        }
+          target: "36",
+        },
       },
       {
         data: {
           source: "80",
-          target: "76"
-        }
+          target: "76",
+        },
       },
       {
         data: {
           source: "22",
-          target: "86"
-        }
+          target: "86",
+        },
       },
       {
         data: {
           source: "86",
-          target: "80"
-        }
+          target: "80",
+        },
       },
       {
         data: {
           source: "95",
-          target: "89"
-        }
+          target: "89",
+        },
       },
       {
         data: {
           source: "89",
-          target: "90"
-        }
+          target: "90",
+        },
       },
       {
         data: {
           source: "102",
-          target: "104"
-        }
+          target: "104",
+        },
       },
       {
         data: {
           source: "80",
-          target: "89"
-        }
+          target: "89",
+        },
       },
       {
         data: {
           source: "104",
-          target: "109"
-        }
+          target: "109",
+        },
       },
       {
         data: {
           source: "115",
-          target: "109"
-        }
+          target: "109",
+        },
       },
       {
         data: {
           source: "121",
-          target: "89"
-        }
+          target: "89",
+        },
       },
       {
         data: {
           source: "121",
-          target: "115"
-        }
+          target: "115",
+        },
       },
       {
         data: {
           source: "121",
-          target: "128"
-        }
+          target: "128",
+        },
       },
       {
         data: {
           source: "128",
-          target: "130"
-        }
+          target: "130",
+        },
       },
       {
         data: {
           source: "141",
-          target: "128"
-        }
+          target: "128",
+        },
       },
       {
         data: {
           source: "128",
-          target: "136"
-        }
+          target: "136",
+        },
       },
       {
         data: {
           source: "136",
-          target: "148"
-        }
+          target: "148",
+        },
       },
       {
         data: {
           source: "148",
-          target: "149"
-        }
+          target: "149",
+        },
       },
       {
         data: {
           source: "149",
-          target: "156"
-        }
+          target: "156",
+        },
       },
       {
         data: {
           source: "156",
-          target: "158"
-        }
+          target: "158",
+        },
       },
       {
         data: {
           source: "130",
-          target: "164"
-        }
+          target: "164",
+        },
       },
       {
         data: {
           source: "164",
-          target: "166"
-        }
+          target: "166",
+        },
       },
       {
         data: {
           source: "178",
-          target: "179"
-        }
+          target: "179",
+        },
       },
       {
         data: {
           source: "184",
-          target: "178"
-        }
+          target: "178",
+        },
       },
       {
         data: {
           source: "178",
-          target: "189"
-        }
+          target: "189",
+        },
       },
       {
         data: {
           source: "141",
-          target: "178"
-        }
+          target: "178",
+        },
       },
       {
         data: {
           source: "104",
-          target: "103"
-        }
+          target: "103",
+        },
       },
       {
         data: {
           source: "10",
-          target: "12"
-        }
+          target: "12",
+        },
       },
       {
         data: {
           source: "164",
-          target: "172"
-        }
+          target: "172",
+        },
       },
       {
         data: {
           source: "22",
-          target: "104"
-        }
-      }
-    ]
-  }
+          target: "104",
+        },
+      },
+    ],
+  },
 };
 
 export default {
@@ -676,14 +676,17 @@ export default {
   data() {
     return {
       config,
-      i: 1
+      i: 1,
     };
   },
-    mounted() {
-    const resizerScript = document.createElement('script')
-    resizerScript.setAttribute('src', 'https://cdn.jsdelivr.net/npm/iframe-resizer@4.3.1/js/iframeResizer.contentWindow.min.js')
-    document.head.appendChild(resizerScript)
-    },
+  mounted() {
+    const resizerScript = document.createElement("script");
+    resizerScript.setAttribute(
+      "src",
+      "https://cdn.jsdelivr.net/npm/iframe-resizer@4.3.1/js/iframeResizer.contentWindow.min.js"
+    );
+    document.head.appendChild(resizerScript);
+  },
   methods: {
     preConfig(cytoscape) {
       console.log("calling pre-config");
@@ -693,8 +696,8 @@ export default {
     afterCreated(cy) {
       // cy: this is the cytoscape instance
       console.log("after created");
-    }
-  }
+    },
+  },
 };
 </script>
 
