fd3b7446284112d603f023e08c0e676c74d094db
MeStrak <call_me_nigel@yahoo.co.uk>
2021-02-13 11:55:57 +0000

    feat: add lots of vuetify components

--- child-app/src/views/Long.vue
+++ child-app/src/views/Long.vue
@@ -1,13 +1,175 @@
 <template>
   <div class="home">
-      This is some long content
     <HelloWorld msg="This is child app 1!!!!" />
-        <HelloWorld msg="This is child app 1!!!!" />
-            <HelloWorld msg="This is child app 1!!!!" />
-                <HelloWorld msg="This is child app 1!!!!" />
+    <v-alert border="top" color="red lighten-2" dark>
+      I'm an alert with a top border and red color
+    </v-alert>
+    <v-alert border="right" color="blue-grey" dark>
+      I'm an alert with a right border and blue-grey color
+    </v-alert>
+    <v-alert border="bottom" color="pink darken-1" dark>
+      I'm an alert with a bottom border and pink color
+    </v-alert>
+    <v-alert border="left" color="indigo" dark>
+      I'm an alert with a border left type info
+    </v-alert>
 
+    <div class="text-center">
+      <v-bottom-sheet v-model="sheet" inset>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="orange" dark v-bind="attrs" v-on="on">
+            Open Inset
+          </v-btn>
+        </template>
+        <v-sheet class="text-center" height="200px">
+          <v-btn class="mt-6" text color="error" @click="sheet = !sheet">
+            close
+          </v-btn>
+          <div class="my-3">This is a bottom sheet using the inset prop</div>
+        </v-sheet>
+      </v-bottom-sheet>
+    </div>
+    <div class="text-center">
+    <v-btn
+      class="ma-2"
+      :loading="loading"
+      :disabled="loading"
+      color="secondary"
+      @click="loader = 'loading'"
+    >
+      Accept Terms
+    </v-btn>
+
+    <v-btn
+      :loading="loading3"
+      :disabled="loading3"
+      color="blue-grey"
+      class="ma-2 white--text"
+      @click="loader = 'loading3'"
+    >
+      Upload
+      <v-icon
+        right
+        dark
+      >
+        mdi-cloud-upload
+      </v-icon>
+    </v-btn>
+
+    <v-btn
+      class="ma-2"
+      :loading="loading2"
+      :disabled="loading2"
+      color="success"
+      @click="loader = 'loading2'"
+    >
+      Custom Loader
+      <template v-slot:loader>
+        <span>Loading...</span>
+      </template>
+    </v-btn>
+
+    <v-btn
+      class="ma-2"
+      :loading="loading4"
+      :disabled="loading4"
+      color="info"
+      @click="loader = 'loading4'"
+    >
+      Icon Loader
+      <template v-slot:loader>
+        <span class="custom-loader">
+          <v-icon light>mdi-cached</v-icon>
+        </span>
+      </template>
+    </v-btn>
+
+    <v-btn
+      :loading="loading5"
+      :disabled="loading5"
+      color="blue-grey"
+      class="ma-2 white--text"
+      fab
+      @click="loader = 'loading5'"
+    >
+      <v-icon dark>
+        mdi-cloud-upload
+      </v-icon>
+    </v-btn>
   </div>
-  
+  <v-carousel v-model="model">
+    <v-carousel-item
+      v-for="(color, i) in colors"
+      :key="color"
+    >
+      <v-sheet
+        :color="color"
+        height="100%"
+        tile
+      >
+        <v-row
+          class="fill-height"
+          align="center"
+          justify="center"
+        >
+          <div class="display-3">
+            Slide {{ i + 1 }}
+          </div>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
+    <div class="text-center">
+    <v-chip
+      class="ma-2"
+    >
+      Default
+    </v-chip>
+
+    <v-chip
+      class="ma-2"
+      color="primary"
+    >
+      Primary
+    </v-chip>
+
+    <v-chip
+      class="ma-2"
+      color="secondary"
+    >
+      Secondary
+    </v-chip>
+
+    <v-chip
+      class="ma-2"
+      color="red"
+      text-color="white"
+    >
+      Red Chip
+    </v-chip>
+
+    <v-chip
+      class="ma-2"
+      color="green"
+      text-color="white"
+    >
+      Green Chip
+    </v-chip>
+      <v-expansion-panels>
+    <v-expansion-panel
+      v-for="(item,i) in 20"
+      :key="i"
+    >
+      <v-expansion-panel-header>
+        Item
+      </v-expansion-panel-header>
+      <v-expansion-panel-content>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
+  </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -16,8 +178,18 @@ import HelloWorld from "../components/HelloWorld.vue"; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  private sheet = false;
+  private model = 0;
+  private colors = [
+        'primary',
+        'secondary',
+        'yellow darken-2',
+        'red',
+        'orange',
+      ];
+}
 </script>
