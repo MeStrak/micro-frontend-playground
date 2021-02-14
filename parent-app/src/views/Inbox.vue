ec4b7c6ddd9309bdfb00c458de3c2f756f268e33
MeStrak <call_me_nigel@yahoo.co.uk>
2021-02-14 10:04:05 +0000

    feat: inbox bottom sheet

--- parent-app/src/views/Inbox.vue
+++ parent-app/src/views/Inbox.vue
@@ -7,11 +7,23 @@
 
           <v-list two-line>
             <template v-for="n in 6">
-              <v-list-item :key="n">
-                <v-list-item-avatar color="grey darken-1"> </v-list-item-avatar>
+              <v-list-item
+                :key="n"
+                @click="
+                  sheet = !sheet;
+                  sheetTitle = n;
+                "
+              >
+                <v-list-item-avatar color="white">
+                  <v-img
+                    :src="require('../assets/logo.svg')"
+                    class="my-3"
+                    contain
+                  />
+                </v-list-item-avatar>
 
                 <v-list-item-content>
-                  <v-list-item-title>Message {{ n }}</v-list-item-title>
+                  <v-list-item-title>Item {{ n }}</v-list-item-title>
 
                   <v-list-item-subtitle>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -26,10 +38,30 @@
         </v-card>
       </v-col>
     </v-row>
+    <div class="text-center">
+      <v-bottom-sheet v-model="sheet" inset>
+        <v-sheet class="text-center">
+          <v-card>
+            <v-card-title class="headline">
+              Viewing Item {{ sheetTitle }}
+            </v-card-title>
+
+            <iframe
+              v-resize.quiet="{ log: true }"
+              width="100%"
+              frameborder="0"
+              :src="childApp"
+            ></iframe>
+          </v-card>
+        </v-sheet>
+      </v-bottom-sheet>
+    </div>
   </v-container>
 </template>
 
 <script>
+import iFrameResize from "iframe-resizer/js/iframeResizer";
+
 export default {
   data: () => ({
     cards: ["Today", "Yesterday"],
@@ -38,8 +70,25 @@ export default {
       ["mdi-inbox-arrow-down", "Inbox"],
       ["mdi-send", "Send"],
       ["mdi-delete", "Trash"],
-      ["mdi-alert-octagon", "Spam"]
-    ]
-  })
+      ["mdi-alert-octagon", "Spam"],
+    ],
+    sheet: false,
+    sheetTitle: "x",
+    childApp: process.env.VUE_APP_CHILD_APP_URL,
+  }),
+  directives: {
+    resize: {
+      bind: function (
+        el,
+        {
+          options = {
+            scrolling: false,
+          },
+        }
+      ) {
+        el.addEventListener("load", () => iFrameResize(options, el));
+      },
+    },
+  },
 };
 </script>
